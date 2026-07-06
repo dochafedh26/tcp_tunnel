349961d9d67f292c85fb966e03996d8babf3f1b6
dochafedh14 <doc.hafedh14@gmail.com>
2026-07-06 20:01:06 +0100

    bump: Version 1.0.61

--- agent/lib/updater.dart
+++ agent/lib/updater.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 class AgentUpdater {
-  static const String currentVersion = '1.0.60';
+  static const String currentVersion = '1.0.61';
   static const String repoOwner = 'dochafedh26';
   static const String repoName = 'tcp_tunnel';
 
